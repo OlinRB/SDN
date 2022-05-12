b556d0b2015896fb348c168866b3bfe3713d0b4a
OlinRB <olinrb@live.com>
2022-05-12 16:58:29 -0400

    finished

--- constants.py
+++ constants.py
@@ -1,34 +1,34 @@
 post_data = {
   "table_entries": [
     {
-      "switch_id": 1,
+      "switch_id": '1',
       "dst_ip": "169.254.173.130",
-      "out_port": 1
+      "out_port": '1'
     },
     {
-      "switch_id": 5,
+      "switch_id": '5',
       "dst_ip": "169.254.173.130",
-      "out_port": 1
+      "out_port": '1'
     },
     {
-      "switch_id": 9,
+      "switch_id": '9',
       "dst_ip": "169.254.173.130",
-      "out_port": 16
+      "out_port": '16'
     },
     {
-      "switch_id": 9,
+      "switch_id": '9',
       "dst_ip": "169.254.20.158",
-      "out_port": 15
+      "out_port": '15'
     },
     {
-      "switch_id": 5,
+      "switch_id": '5',
       "dst_ip": "169.254.20.158",
-      "out_port": 3
+      "out_port": '3'
     },
     {
-      "switch_id": 1,
+      "switch_id": '1',
       "dst_ip": "169.254.20.158",
-      "out_port": 2
+      "out_port": '2'
     }
   ]
 }
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Assignment link: http://ceskalka.w3.uvm.edu/265/sdn-assignment/
 
 import requests
-from constants import*
 from dijkstar import Graph, find_path
 import sys
+import os
 
 class Node:
 
@@ -15,26 +15,19 @@ class Node:
         self.port_num = link_info[2]
         self.key = str(self.node_name) + "_" + str(self.dest_name)
 
-    def get_set(self):
-        return {self.node_name, self.dest_name}
-
 
 def forwarding_table(network):
+    # Create graph and add edges
     graph = Graph()
     for node in network:
         graph.add_edge(node.node_name, node.dest_name, 1)
 
-    # Store host links
-    host_nodes = []
-    entries = []
+    # Find host nodes and directly connected nodes
     src_dest = {}
-    all_links = {}
     for node in network:
         if isinstance(node.node_name, str) or isinstance(node.dest_name, str):
             src_dest[node.node_name] = node.node
-        all_links[node.key] = node
 
-    #print(src_dest)
     hosts = ["169.254.240.121", "169.254.173.130", "169.254.20.158"]
 
     paths = []
@@ -43,27 +36,72 @@ def forwarding_table(network):
             if src_node != dest:
                 src = src_dest[src_node][1]
                 paths.append(find_path(graph, src, dest).nodes)
-                #paths.append(find_path(graph, dest, src).nodes)
 
-    for path in paths:
-        print(path)
-    print(find_path(graph, 9, "169.254.20.158").nodes)
+    # Retrun paths -> each sublist is a path in the network
+    return paths
 
 
+def format_data(fw_table, all_links):
+    # Convert data from 2D list from forwarding_table() to REST format
+    table_data = []
+    for path in fw_table:
+        dest = path[len(path)-1]
+        while len(path) > 1:
+            path_data = {}
+            node = all_links[str(path[0]) + "_" + str(path[1])]
+            port = node.port_num
+            path = path[1:]
+            path_data["switch_id"] = node.node_name
+            path_data["dst_ip"] = dest
+            path_data["out_port"] = port
+            table_data.append(path_data)
+
+    formatted_table = {}
+    formatted_table["table_entries"] = table_data
+    return formatted_table
 
 def main():
+    # Pip install library
+    os.system("pip install dijkstar")
+
+    # Get cmd line args
     send_location = sys.argv[1]
     topology_action = sys.argv[2]
-    topology = requests.get(("http://{}:2222/get_topology/{}".format(send_location, topology_action)))
+    virtual = True
+    if virtual:
+        topology = requests.get(("http://{}:2222/get_topology/{}".format(send_location, topology_action)))
+    else:
+        topology = requests.get("132.198.11.11:2222/get_topology")
     nodes = topology.json()['connected']
 
-    formatted_tables = requests.post("http://www.goatgoose.com:2222/set_tables/topology1", post_data).json()
+    # Append network
     network = []
     for node in nodes:
         network.append(Node(node))
-    #print(nodes)
+
+    # Get forwarding table
     fw_table = forwarding_table(network)
-    print(formatted_tables)
+
+    # format forwarding table
+    all_links = {}
+    for node in network:
+        all_links[node.key] = node
+    formatted_table = format_data(fw_table, all_links)
+
+    # Send formatted forwarding table
+    if virtual:
+        formatted_tables = requests.post("http://www.goatgoose.com:2222/set_tables/topology1", json=formatted_table)
+    else:
+        formatted_tables = requests.post("132.198.11.11:2222/set_tables", json=formatted_table)
+
+    result = formatted_tables.json()["error"]
+    if result == None:
+        print("Successfully configured forwarding table")
+        print(formatted_tables.json())
+
+    else:
+        print("Error...")
+        print(formatted_tables.json())
 
 if __name__ == '__main__':
     main()
